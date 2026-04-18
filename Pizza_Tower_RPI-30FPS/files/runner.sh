06aa31f1713879a4f14d10a5254d1ba02c991fbd
Marten (AdventuresOfMartenGuy) <braslavskyapollo@gmail.com>
2026-04-17 21:33:59 -0400

    Enhance runner.sh with resolution check and loading UI
    Add resolution check and loading message for Pizza Tower launcher

--- Pizza_Tower_RPI-30FPS/files/runner.sh
+++ Pizza_Tower_RPI-30FPS/files/runner.sh
@@ -1,3 +1,32 @@
 VERSION=5
+#!/bin/bash
 
-MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower30/Game/PizzaTower.exe -debug
+if command -v xrandr > /dev/null; then
+    current_v_res=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d'x' -f2)
+else
+    current_v_res=$(WAYLAND_DISPLAY="wayland-1" wlr-randr | grep '*' | awk '{print $1}' | cut -d'x' -f2 | head -n1)
+fi
+
+echo "Current vertical resolution: ${current_v_res}p"
+if [ "$current_v_res" -le 720 ]; then
+    yad --pulsate --text="  Loading...  (Note If You Want 30 FPS Set The System To Be Above 1280x720)." \
+    --width=200 --undecorated --fixed --skip-taskbar \
+    --no-buttons --on-top --center &    
+    yad_id=$!
+    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower/Game/PizzaTower.exe -debug &
+else
+    yad --pulsate --text="  Loading...  (Note If You Want 30 FPS Set The System To Be Above 1280x720)." \
+    --width=200 --undecorated --fixed --skip-taskbar \
+    --no-buttons --on-top --center &    
+    yad_id=$!
+    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower30/Game/PizzaTower.exe -debug &
+fi
+
+while ! pgrep -fi "PizzaTower|wine|winedevice" > /dev/null; do
+    sleep 1
+    if ! kill -0 $yad_id 2>/dev/null; then exit; fi
+done
+
+sleep 6
+
+kill $yad_id
