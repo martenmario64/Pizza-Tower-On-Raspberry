a33531e963f11252e1b6d62678670fa3b04d964e
Marten (AdventuresOfMartenGuy) <braslavskyapollo@gmail.com>
2026-04-18 12:01:11 -0400

    Improve loading messages and process management
    Refactor loading messages and process handling in runner.sh.

--- Pizza_Tower_RPI-30FPS/files/runner.sh
+++ Pizza_Tower_RPI-30FPS/files/runner.sh
@@ -7,26 +7,26 @@ else
     current_v_res=$(WAYLAND_DISPLAY="wayland-1" wlr-randr | grep '*' | awk '{print $1}' | cut -d'x' -f2 | head -n1)
 fi
 
-echo "Current vertical resolution: ${current_v_res}p"
 if [ "$current_v_res" -le 720 ]; then
+    sleep 4 &
     yad --pulsate --text="  Loading...  (Note If You Want 30 FPS Set The System To Be Above 1280x720)." \
     --width=200 --undecorated --fixed --skip-taskbar \
-    --no-buttons --on-top --center &    
+    --no-buttons --on-top --timeout=4 --center &    
     yad_id=$!
-    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower/Game/PizzaTower.exe -debug &
 else
+    sleep 4 &
     yad --pulsate --text="  Loading...  (Note If You Want 45 FPS Set The System To Be Below 1280x960)." \
     --width=200 --undecorated --fixed --skip-taskbar \
-    --no-buttons --on-top --center &    
+    --no-buttons --on-top --timeout=4 --center &    
     yad_id=$!
-    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower30/Game/PizzaTower.exe -debug &
 fi
 
-while ! pgrep -fi "PizzaTower|wine|winedevice" > /dev/null; do
-    sleep 1
-    if ! kill -0 $yad_id 2>/dev/null; then exit; fi
-done
+#kill $yad_id
 
-sleep 6
+echo "Current vertical resolution: ${current_v_res}p"
+if [ "$current_v_res" -le 720 ]; then
+    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower/Game/PizzaTower.exe -debug
+else
+    MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatower30/Game/PizzaTower.exe -debug
+fi
 
-kill $yad_id
