38ee73c89d045f31d8b44a42f54ed4eb1d0a7b2c
Marten (AdventuresOfMartenGuy) <braslavskyapollo@gmail.com>
2026-04-19 00:38:40 -0400

    Add terminal check and launch command in runner.sh

--- demofiles/runner.sh
+++ demofiles/runner.sh
@@ -1,4 +1,10 @@
 #runner.sh contents
+
 VERSION=6
+
+if [ "$1" != "--in-terminal" ]; then
+    lxterminal -e "bash -c '$0 --in-terminal; exec bash'"
+    exit
+fi
 
 MESA_GL_VERSION_OVERRIDE=3.3 MESA_GLSL_VERSION_OVERRIDE=330 BOX64_LIBGL=libGL.so.1 wine ~/.martenapps/pizzatowermm64demo/Game/PizzaTower_GM2.exe
